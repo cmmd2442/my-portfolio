048c52b70ae5fe400aeb0cf00c7ae3301c63f9d4
charlie <charlie@charlie.chadzi.com>
2023-08-23 19:00:27 -0400

    test

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,7 @@ module.exports = {
   entry: './js/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    hashFunction: "sha256"
+    filename: 'bundle.js'
   },
   module: {
     rules: [{
