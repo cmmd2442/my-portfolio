3a1453e848d924e15948b229945bf30adec7e86c
charlie <charlie@charlie.chadzi.com>
2023-08-25 12:52:00 -0400

    test

---
+++ js/example-work.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ExampleWork extends React.Component {
+
+  render(){
+    return (
+      <section>It works!</section>
+
+
+    )
+  }	  
+}
+
+export default ExampleWork;
--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
+ReactDOM.render(<ExampleWork />,document.getElementById('example-work'));
+
 
 console.log("loaded react-dom");
